36efc2c608c53fe89cf0de93d07924ed9b87b33e
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-31 17:16:39 +0600

    Routing all pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Route, Switch} from 'react-router-dom';
 
 import Home from "./pages/HomePage";
 import About from "./pages/AboutPage";
 import Products from "./pages/ProductsPage";
 import Contact from "./pages/ContactPage";
 import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage";
 import Default from "./pages/Default.js";
 
 class App extends Component {
     render() {
-        return <h1>hello from tech store</h1>;
+        return <>
+            {/* navbar, sidebar, cart, footer*/}
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/products" exact component={Products}/>
+                <Route path="/product/:id" component={SingleProduct}/>
+                <Route path="/cart" exact component={Cart}/>
+                <Route component={Default}/>
+            </Switch>
+        </>;
     }
 }
 
