b3c871268725b3657e282002ba0db8c2a5b9a539
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-04 15:30:27 +0600

    Product Componenet View 🤩

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from "react";
 import Product from "../Product";
-import {Link} from 'react-router-dom';
+import {Link} from "react-router-dom";
 import Title from "../Title";
-import {ProductConsumer} from '../../context';
+import {ProductConsumer} from "../../context";
 
 export default function Featured() {
     return (
         <section className="py-5">
             <div className="container">
 
-                {/*TITLE*/}
-                <Title title="Featured Product" center="true"/>
+                {/* TITLE */}
+                <Title title="featured products" center="true"/>
 
-                {/*PRODUCT*/}
+                {/* PRODUCTS */}
                 <div className="row">
                     <ProductConsumer>
                         {value => {
                             const {featuredProducts} = value;
 
                             return featuredProducts.map(product => (
-                                <Product
-                                    key={product.id}
-                                    product={product}
-                                />
-                                
-                            ))
+                                <Product key={product.id} product={product}/>
+                            ));
                         }}
                     </ProductConsumer>
-
+                </div>
+                <div className="row mt-5">
+                    <div className="col text-center">
+                        <Link to="/products" className="main-link">
+                            our products
+                        </Link>
+                    </div>
                 </div>
             </div>
         </section>
     );
-};
-
+}
