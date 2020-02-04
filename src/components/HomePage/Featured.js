f5378ecac39b2394b7aa6f3998c009a02aea0092
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-04 14:35:43 +0600

    Added Featured Products Section

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Product from "../Product";
+import {Link} from 'react-router-dom';
+import Title from "../Title";
+import {ProductConsumer} from '../../context';
 
-const Featured = () => {
+export default function Featured() {
     return (
-        <div>
-            Hello From Featured
-        </div>
+        <section className="py-5">
+            <div className="container">
+
+                {/*TITLE*/}
+                <Title title="Featured Product" center="true"/>
+
+                {/*PRODUCT*/}
+                <div className="row">
+                    <ProductConsumer>
+                        {value => {
+                            const {featuredProducts} = value;
+
+                            return featuredProducts.map(product => (
+                                <Product
+                                    key={product.id}
+                                    product={product}
+                                />
+                                
+                            ))
+                        }}
+                    </ProductConsumer>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
-export default Featured;
---
+++ src/components/Product.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Product() {
+    return (
+        <div>
+            Product Component
+        </div>
+    );
+};
+
