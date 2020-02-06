6a0404f9ef554ca9cd23ba8cd2d24b4af2a1907d
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-06 11:36:56 +0600

    All Products Page is Done

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {ProductConsumer} from '../../context';
+import Title from "../Title";
+import Product from "../Product";
+// import styled from "styled-components";
 
 export default function Products() {
     return (
-        <div>
-            Our Products Page
-        </div>
+        <ProductConsumer>
+            {value => {
+                const {filteredProducts} = value;
+                return <section className="py-5">
+                    <div className="container">
+
+                        {/*TITLE*/}
+                        <Title title="Our Products" center/>
+
+                        {/*ALL OUR PRODUCTS*/}
+                        <div className="row py-3">
+                            {filteredProducts.map(product => {
+                                return <Product
+                                    key={product.id}
+                                    product={product}
+                                />
+                            })}
+
+                        </div>
+                    </div>
+
+                </section>
+            }}
+        </ProductConsumer>
     );
 };
