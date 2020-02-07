4707202c0887c0d820cd6f7a62b4c5ff0cf21ebc
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-07 13:59:17 +0600

    CartList Component Is Done

--- src/components/CartPage/CartList.js
+++ src/components/CartPage/CartList.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import {ProductConsumer} from '../../context';
 import CartItem from "./CartItem";
 
 export default function CartList() {
     return (
-        <div>
-            CartList
-            <CartItem/>
+        <div className="container-fluid">
+
+            {/*ROW*/}
+            <div className="row">
+                <div className="col">
+                    <ProductConsumer>
+                        {
+                            value => {
+                                const {cart, increment, decrement, removeItem} = value;
+                                if (cart.length === 0) {
+                                    return (
+                                        <h1 className="text-title text-center my-4 text-warning">
+                                            Your Cart is Empty
+                                        </h1>
+                                    );
+                                }
+                                return (
+                                    <div>
+                                        {cart.map(item => (
+                                            <CartItem
+                                                key={item.id}
+                                                cartItem={item}
+                                                increment={increment}
+                                                decrement={decrement}
+                                                removeItem={removeItem}
+                                            />
+                                        ))}
+                                    </div>
+                                );
+                            }
+                        }
+                    </ProductConsumer>
+                </div>
+
+            </div>
+            {/*<CartItem/>*/}
         </div>
     );
 };
